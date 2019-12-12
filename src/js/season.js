b671f7d50da785c0a4099c47d168d89d2caea61a
Dmitry Semko <dimaisprogrammer@gmail.com>
2019-12-12 16:07:15 +0300

    adding barcodes

--- src/js/season.js
+++ src/js/season.js
@@ -79,8 +79,6 @@ export const initSnowFlakesNew = () => {
 
         if (event.touches.length == 1) {
 
-            event.preventDefault();
-
             mouseX = event.touches[0].pageX - windowHalfX;
             mouseY = event.touches[0].pageY - windowHalfY;
         }
