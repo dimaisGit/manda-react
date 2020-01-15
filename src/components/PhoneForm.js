0800f094faaecbfeab5f0129a03987d97362aed6
Александра Чернышева <ach@worldfashion.pro>
2020-01-15 17:16:28 +0300

    fixed validation

--- src/components/PhoneForm.js
+++ src/components/PhoneForm.js
@@ -1,27 +1,34 @@
 import React from 'react'
 import { Field, reduxForm, SubmissionError, change, stopSubmit } from "redux-form";
 import { RenderField } from "./RenderField";
-import $ from 'jquery'
+import Countdown from 'react-countdown';
+
+
+
 
 
 
 class PhoneForm extends React.Component {
-   
+    constructor(props){
+        super(props)
+        this.state ={
+            timer: true
+        }
+    }
     onHandleSubmit = values => {
         if (!values.userPhone) {
             throw new SubmissionError({ userPhone: 'Укажите корректный номер', _error: 'Login failed!' })
         }
-        // if (this.props.codeSent && !values.userCode) {
-        //     throw new SubmissionError({ userCode: 'Введите полученный код', _error: 'Login failed!' })
-        // }
+       
         if (!this.props.codeSent || !values.userCode)
             this.props.handleSendCode('+7' + values.userPhone)
         else
             this.props.handleCheckCode('+7' + values.userPhone, values.userCode)
     }
-    makeSmth = (e) => {
-        e.preventDefault()
-        console.log(123)
+    resetTimer = () => {
+        this.setState({
+            timer: false
+        })
     }
     componentDidUpdate(prevProps, prevState, snapshot) {
         const { verifyCodeError, dispatch } = this.props
@@ -32,19 +39,33 @@ class PhoneForm extends React.Component {
     }
 
     render() {
-        const { handleSubmit, submitting, codeSent, sendError } = this.props
+        const { handleSubmit, submitting, codeSent, sendError, state } = this.props
+        const renderer = ({ hours, minutes, seconds, completed }) => {
+            if (completed) {
+              // Render a complete state
+              return <Completionist/>;
+            } else {
+              // Render a countdown
+              return (this.state.timer ? <span className="formCodeAgain">Код успешно отправлен.<br/>Повторная отправка возможна через {seconds} сек.</span> : 
+              <span className="formCodeAgain">Код был отправлен повторно </span>)
+            }
+          };
+        const Completionist = () => (
+            <button type='submit' disabled={submitting} className="formCodeAgain" onClick={this.resetTimer}> Отправить код повторно </button>
+            );
+        
         return (
             <>
             <form onSubmit={handleSubmit(this.onHandleSubmit)}>
                 <div className="cancelBut"></div>
                 <h2>Введите номер телефона</h2>
                 <Field name="userPhone" component={RenderField} type="tel" placeholder='9990000000' index='1' pattern="[0-9]{10}"/>
-                {codeSent && <p className="formCodeEnt">Введите код</p>}
+                {codeSent && <p className="formCodeEnt">Введите код из смс:</p>}
                 {codeSent && <Field name='userCode' component={RenderField} placeholder=" " />}
-                {codeSent && <button type='submit' disabled={submitting} className="formCodeAgain">Отправить код повторно </button>}
+                {codeSent && <Countdown date={Date.now() + 5000}  intervalDelay={1000} precision={.3} renderer={renderer}></Countdown>}
                 {sendError && <div className='error'>{sendError}</div>}
                 <div className="centerBut">
-                    <button type="submit" disabled={submitting}>{!codeSent ? 'Отправить код' : 'Подтвердить код'}</button>
+                    <button type="submit" disabled={submitting}>{!codeSent ? 'Получить код' : 'Подтвердить код'}</button>
                 </div>
             </form>
             </>
